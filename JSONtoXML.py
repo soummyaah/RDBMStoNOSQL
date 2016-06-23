1cb72d255a408bbc0ab601e0799493ef86b4a085
Rishikesh <rishikksh20@users.noreply.github.com>
2016-06-23 18:50:58 +0530

    Adding some comment for better understanding

--- JSONtoXML.py
+++ JSONtoXML.py
@@ -3,9 +3,9 @@
 Description: Simple way to convert JSON to xml in python using url request
 """
 
-import requests  # pip install requests
+import requests  # pip install requests only for web request 
 import json
-import dicttoxml #pip install dicttoxml
+import dicttoxml #pip install dicttoxml for convert python dictionary to xml
 obj = requests.get('https://github.com/timeline.json').json()
 xml = dicttoxml.dicttoxml(obj)
 print(xml)
